f60001bcb559c0bd546e9f982e453e165cbb0304
Filtration <oracrin@gmail.com>
2017-01-20 19:01:07 +0000

    Fixed trailing and leading ws

--- twitter_checker.py
+++ twitter_checker.py
@@ -2,9 +2,9 @@
 # _*_ coding: utf-8 _*_
 
 # Check if twitter usernames exist or are available
-
 import argparse as ap
 import requests
+import time
 
 def args():
     """ Get the arguments passed from the CLINE """
@@ -20,19 +20,19 @@ def checker():
     usernames = open(argslist.u, "r")
     lines = usernames.readlines()
     usernames.close()
-
+    checked = open("saved.txt", "a+")
     for line in lines:
-        url = "https://twitter.com/" + line
+        url = "https://twitter.com/" + line.strip()
         check = requests.get(url)
 
         #a simple 404 will tell us it doesn't exist
         if check.status_code == 404:
             message = '%s is avaialble\n' % line.strip()
+        elif check.status_code == 400:
+            message = '%s is a bad formatted username. Alphanumeric only\n' % line.strip()
         else:
             message = '%s is taken\n' % line.strip()
-        
-        print line
+
         #save the results
-        checked = open("saved.txt", "a+")
         checked.write(message)
 checker()
